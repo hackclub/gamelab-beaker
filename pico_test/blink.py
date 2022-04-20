d651d8f3a881040f8c637bbc465c0ec0c393b9a7
leomcelroy <leomcelroy@gmail.com>
2022-04-20 14:39:15 -0400

    adding micropython screen code

---
+++ pico_test/blink.py
@@ -0,0 +1,9 @@
+# Write your code here :-)
+from machine import Pin, Timer
+led = Pin(25, Pin.OUT)
+timer = Timer()
+
+def blink(timer):
+    led.toggle()
+
+timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
---
+++ pico_test/st7735/graphicstest.py
@@ -0,0 +1,40 @@
+from ST7735 import TFT
+from sysfont import sysfont
+from machine import SPI,Pin
+import time
+import math
+spi = SPI(1, baudrate=20000000, polarity=0, phase=0,
+          sck=Pin(10), mosi=Pin(11), miso=None)
+tft=TFT(spi,16,17,18)
+tft.initr()
+tft.rgb(True)
+
+def testlines(color):
+    print("testing lines")
+    tft.fill(TFT.BLACK)
+    for x in range(0, tft.size()[0], 6):
+        tft.line((0,0),(x, tft.size()[1] - 1), color)
+    for y in range(0, tft.size()[1], 6):
+        tft.line((0,0),(tft.size()[0] - 1, y), color)
+
+    tft.fill(TFT.BLACK)
+    for x in range(0, tft.size()[0], 6):
+        tft.line((tft.size()[0] - 1, 0), (x, tft.size()[1] - 1), color)
+    for y in range(0, tft.size()[1], 6):
+        tft.line((tft.size()[0] - 1, 0), (0, y), color)
+
+    tft.fill(TFT.BLACK)
+    for x in range(0, tft.size()[0], 6):
+        tft.line((0, tft.size()[1] - 1), (x, 0), color)
+    for y in range(0, tft.size()[1], 6):
+        tft.line((0, tft.size()[1] - 1), (tft.size()[0] - 1,y), color)
+
+    tft.fill(TFT.BLACK)
+    for x in range(0, tft.size()[0], 6):
+        tft.line((tft.size()[0] - 1, tft.size()[1] - 1), (x, 0), color)
+    for y in range(0, tft.size()[1], 6):
+        tft.line((tft.size()[0] - 1, tft.size()[1] - 1), (0, y), color)
+
+    print("testing lines finished")
+
+testlines(TFT.RED)
