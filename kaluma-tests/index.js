067c7f14912161395ec27c793d0076f936291f9c
leomcelroy <leomcelroy@gmail.com>
2022-04-21 21:45:01 -0400

    more drawing tests

--- kaluma-tests/index.js
+++ kaluma-tests/index.js
@@ -28,17 +28,19 @@ const spi = board.spi(1, spiOptions);
 st7735.setup(spi, options);
 const gc = st7735.getContext("buffer");
 
-const color = gc.color16(255, 255, 255);
+const color = gc.color16(255, 100, 255);
 
 
 let size = 10;
+let delta = 5;
 setInterval(() => {
-  gc.fillScreen(color)
+  gc.fillScreen(color);
   // gc.drawRect(0, 0, size, 160);
   gc.drawCircle(128/2, 160/2, size);
   gc.display();
-  size += 10;
-}, 200);
+  size += delta;
+  if (size === 100 || size === 0) delta *= -1;
+}, 30);
 
 
 // gc.display(); // must call if buffered graphic context
